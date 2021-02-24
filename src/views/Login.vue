a150f05d5bd9aaa6723d59009863ef52f751f24f
Jose L. Guzman Santiago <deckblank@gmail.com>
2021-02-23 19:34:25 -0500

    amplify auth

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,8 @@
 <template>
-    
+    <amplify-authenticator>
+    <!-- The rest of your app code -->
+    <amplify-sign-out></amplify-sign-out>
+  </amplify-authenticator>
 </template>
 <script>
 export default {
