abccd243aa11fb6d6b773dfa4456d8d03b0563b4
Jose L. Guzman Santiago <deckblank@gmail.com>
2021-02-23 19:20:28 -0500

    amplify configurado

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@aws-amplify/ui-components/loader';
 
+Amplify.configure(aws_exports);
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
 const app = createApp(App);
 app.config.globalProperties.$ENV = process.env;
 app.use(router).mount("#app");
